f717e646ab9138107e5c69ce559701c816a37c41
petar19992 <petar.ljubic@elfak.rs>
2016-10-11 01:10:15 +0200

    RV1 - Napravljen login i register

--- app/src/main/java/company/drink/com/drinkingsveeg/MainActivity.java
+++ app/src/main/java/company/drink/com/drinkingsveeg/MainActivity.java
@@ -1,21 +1,30 @@
 package company.drink.com.drinkingsveeg;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.constraint.ConstraintLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.List;
+
+import company.drink.com.drinkingsveeg.helpers.Constants;
+
+public class MainActivity extends Activity implements View.OnClickListener {
 
     private ConstraintLayout activityMain;
     private EditText email;
     private EditText password;
     private Button singIn;
     private TextView register;
+    private ProgressBar progressBar;
 
     /**
      * Find the Views in the layout<br />
@@ -29,21 +38,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         password = (EditText)findViewById( R.id.password );
         singIn = (Button)findViewById( R.id.singIn );
         register = (TextView)findViewById( R.id.textView );
+        progressBar = (ProgressBar) findViewById( R.id.progressBar );
 
         singIn.setOnClickListener( this );
     }
-
-    /**
-     * Handle button click events<br />
-     * <br />
-     * Auto-created on 2016-10-10 00:19:47 by Android Layout Finder
-     * (http://www.buzzingandroid.com/tools/android-layout-finder)
-     */
     @Override
     public void onClick(View v) {
-        if ( v == singIn ) {
+        if ( v == singIn )
+        {
+            List<Pair<String,String>> params=new ArrayList<>();
+            params.add(new Pair<String, String>("email",email.getText().toString()));
+            params.add(new Pair<String, String>("password",password.getText().toString()));
+            Constants.getInstance().POST("http://drinking.sveeg.com/api/login", params, new Constants.IGetInterface() {
+                @Override
+                public void onStart() {
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onFinish(String result) {
+                    Log.i("result",result);
+                    progressBar.setVisibility(View.GONE);
+                }
+            });
         }
     }
+    //Value null{"token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMsImlzcyI6Imh0dHA6XC9cL2RyaW5raW5nLnN2ZWVnLmNvbVwvYXBpXC9sb2dpbiIsImlhdCI6MTQ3NjEzOTk2NSwiZXhwIjoxNDc2MTQzNTY1LCJuYmYiOjE0NzYxMzk5NjUsImp0aSI6ImUxZTgyOGNlZTc3ZjkzNTcxYTFiMzM4NGI5NmJiMjc1In0.bzukFmVJJ16Dlh2J-KU1w5CURn26gz7FtnfajFdg1Xg"}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/company/drink/com/drinkingsveeg/RegisterActivity.java
+++ app/src/main/java/company/drink/com/drinkingsveeg/RegisterActivity.java
@@ -1,21 +1,21 @@
 package company.drink.com.drinkingsveeg;
 
-import android.provider.SyncStateContract;
-import android.support.constraint.ConstraintLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import company.drink.com.drinkingsveeg.helpers.Constants;
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+public class RegisterActivity extends Activity implements View.OnClickListener {
 
     private ConstraintLayout activityRegister;
     private EditText name;
@@ -23,6 +23,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private EditText password;
     private EditText retypePassword;
     private Button button;
+    private ProgressBar progressBar;
 
     private void findViews() {
         activityRegister = (ConstraintLayout)findViewById( R.id.activity_register );
@@ -31,6 +32,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         password = (EditText)findViewById( R.id.editText3 );
         retypePassword = (EditText)findViewById( R.id.editText4 );
         button = (Button)findViewById( R.id.button );
+        progressBar = (ProgressBar) findViewById( R.id.progressBar );
 
         button.setOnClickListener( this );
     }
@@ -46,16 +48,18 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             Constants.getInstance().POST("http://drinking.sveeg.com/api/register", params, new Constants.IGetInterface() {
                 @Override
                 public void onStart() {
-
+                    progressBar.setVisibility(View.VISIBLE);
                 }
 
                 @Override
                 public void onFinish(String result) {
                     Log.i("result",result);
+                    progressBar.setVisibility(View.GONE);
                 }
             });
         }
     }
+    //null{"token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjUsImlzcyI6Imh0dHA6XC9cL2RyaW5raW5nLnN2ZWVnLmNvbVwvYXBpXC9yZWdpc3RlciIsImlhdCI6MTQ3NjEzOTY4NCwiZXhwIjoxNDc2MTQzMjg0LCJuYmYiOjE0NzYxMzk2ODQsImp0aSI6IjZhNzIwYzM4NzFmZmEwNjc3MzEyMWZjZDA3NWY5ZDQyIn0.7xj-441xvMf4XkEvypAi-Ms1dGe2Gk5dXaedljibLOU"}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
